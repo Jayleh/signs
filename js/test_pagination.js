8a6bab13f1971d749b682b01d636840d9d8d123c
Jayleh <jaylehyun@gmail.com>
2018-05-01 16:44:57 -0700

    edit pagination

--- js/test_pagination.js
+++ js/test_pagination.js
@@ -1,10 +1,10 @@
 function renderTable() {
-    let $tBody = document.querySelector("tbody");
+    let $tBody = document.querySelector('tbody');
 
     for (let i = 0, ii = dataSet.length; i < ii; i++) {
         // Insert row and assign to variable
         let $row = $tBody.insertRow(i);
-        // $row.setAttribute("class", "ufo-table-row");
+        // $row.setAttribute('class', 'ufo-table-row');
 
         // Grab each sighting (indices) from dataSet
         let sighting = dataSet[i];
@@ -23,39 +23,50 @@ function renderTable() {
             $cell.title = $cell.innerHTML;
         }
 
-        // a break for testing
-        if (i === 499) {
+        // Break for testing
+        if (i === 2499) {
             break;
         }
     }
 }
 
+
+renderTable();
+
+
 function filterDate() {
     let $input, filter, $table, $tr;
 
-    $input = document.querySelector("#searchDate");
+    $input = document.querySelector('#searchDate');
     filter = $input.value;
-    $table = document.querySelector("table");
-    $tr = $table.querySelectorAll("tr");
+    $table = document.querySelector('table');
+    $tr = $table.querySelectorAll('tr');
 
     for (let i = 0, ii = $tr.length; i < ii; i++) {
 
-        let $td = $tr[i].querySelectorAll("td")[0];
+        let $td = $tr[i].querySelectorAll('td')[0];
         if ($td) {
             if ($td.innerHTML.indexOf(filter) > -1) {
-                $tr[i].style.display = "";
+                $tr[i].style.display = '';
             }
             else {
-                $tr[i].style.display = "none";
+                $tr[i].style.display = 'none';
             }
         }
     }
 }
 
-renderTable();
-
 
 $(document).ready(() => {
+
+    // $("#searchDate").on("keyup", () => {
+    //     let value = $(this).val();
+
+    //     $("#ufo-table tbody tr").filter(() => {
+    //         $(this).toggle($(this).text().indexOf(value) > -1)
+    //     });
+    // });
+
     // Returns an array of maxLength (or less) page numbers
     // where a 0 in the returned array denotes a gap in the series.
     // Parameters:
@@ -63,26 +74,26 @@ $(document).ready(() => {
     //   page:           current page
     //   maxLength:      maximum size of returned array
     function getPageList(totalPages, page, maxLength) {
-        if (maxLength < 5) throw "maxLength must be at least 5";
+        if (maxLength < 5) throw 'maxLength must be at least 5';
 
         function range(start, end) {
             return Array.from(Array(end - start + 1), (_, i) => i + start);
         }
 
-        var sideWidth = maxLength < 9 ? 1 : 2;
-        var leftWidth = (maxLength - sideWidth * 2 - 3) >> 1;
-        var rightWidth = (maxLength - sideWidth * 2 - 2) >> 1;
+        let sideWidth = maxLength < 9 ? 1 : 2;
+        let leftWidth = (maxLength - sideWidth * 2 - 3) >> 1;
+        let rightWidth = (maxLength - sideWidth * 2 - 2) >> 1;
         if (totalPages <= maxLength) {
             // no breaks in list
             return range(1, totalPages);
         }
-        if (page <= maxLength - sideWidth - 1 - rightWidth) {
+        else if (page <= maxLength - sideWidth - 1 - rightWidth) {
             // no break on left of page
             return range(1, maxLength - sideWidth - 1)
                 .concat([0])
                 .concat(range(totalPages - sideWidth + 1, totalPages));
         }
-        if (page >= totalPages - sideWidth - 1 - rightWidth) {
+        else if (page >= totalPages - sideWidth - 1 - rightWidth) {
             // no break on right of page
             return range(1, sideWidth)
                 .concat([0])
@@ -98,67 +109,74 @@ $(document).ready(() => {
 
     $(function () {
         // Number of items and limits the number of items per page
-        var numberOfItems = $("#ufo-table tbody tr").length;
-        var limitPerPage = 50;
+        let numberOfItems = $('#ufo-table tbody tr').length;
+        // let numberOfItems = $('#ufo-table tbody tr:''').length;
+        // let numberOfItems = $('#ufo-table tbody tr').filter(function () {
+        //     return $(this).css('display') !== 'none';
+        // }).length;
+
+        let limitPerPage = 20;
         // Total pages rounded upwards
-        var totalPages = Math.ceil(numberOfItems / limitPerPage);
+        let totalPages = Math.ceil(numberOfItems / limitPerPage);
         // Number of buttons at the top, not counting prev/next,
         // but including the dotted buttons.
         // Must be at least 5:
-        var paginationSize = 7;
-        var currentPage;
+        let paginationSize = 7;
+        let currentPage;
 
         function showPage(whichPage) {
             if (whichPage < 1 || whichPage > totalPages) {
                 return false;
             }
             currentPage = whichPage;
-            $("#ufo-table tbody tr").hide()
-                .slice((currentPage - 1) * limitPerPage,
-                    currentPage * limitPerPage).show();
+            $('#ufo-table tbody tr').hide()
+                .slice((currentPage - 1) * limitPerPage, currentPage * limitPerPage).show();
+
             // Replace the navigation items (not prev/next):            
-            $(".pagination li").slice(1, -1).remove();
+            $('.pagination li').slice(1, -1).remove();
             getPageList(totalPages, currentPage, paginationSize).forEach(item => {
-                $("<li>").addClass("page-item")
-                    .addClass(item ? "current-page" : "disabled")
-                    .toggleClass("active", item === currentPage).append(
-                        $("<a>").addClass("page-link").attr({
-                            href: "javascript:void(0)"
-                        }).text(item || "...")
-                    ).insertBefore("#next-page");
+                $('<li>').addClass('page-item')
+                    .addClass(item ? 'current-page' : 'disabled')
+                    .toggleClass('active', item === currentPage).append(
+                        $('<a>').addClass('page-link').attr({
+                            href: 'javascript:void(0)'
+                        }).text(item || '...')
+                    ).insertBefore('#next-page');
             });
             // Disable prev/next when at first/last page:
-            $("#previous-page").toggleClass("disabled", currentPage === 1);
-            $("#next-page").toggleClass("disabled", currentPage === totalPages);
+            $('#previous-page').toggleClass('disabled', currentPage === 1);
+            $('#next-page').toggleClass('disabled', currentPage === totalPages);
             return true;
         }
 
         // Include the prev/next buttons:
-        $(".pagination").append(
-            $("<li>").addClass("page-item").attr({ id: "previous-page" }).append(
-                $("<a>").addClass("page-link").attr({
-                    href: "javascript:void(0)"
-                }).text("Prev")
+        $('.pagination').append(
+            $('<li>').addClass('page-item').attr({ id: 'previous-page' }).append(
+                $('<a>').addClass('page-link').attr({
+                    href: 'javascript:void(0)'
+                }).text('Prev')
             ),
-            $("<li>").addClass("page-item").attr({ id: "next-page" }).append(
-                $("<a>").addClass("page-link").attr({
-                    href: "javascript:void(0)"
-                }).text("Next")
+            $('<li>').addClass('page-item').attr({ id: 'next-page' }).append(
+                $('<a>').addClass('page-link').attr({
+                    href: 'javascript:void(0)'
+                }).text('Next')
             )
         );
+
         // Show the page links
-        $("#ufo-table").show();
+        $('#ufo-table').show();
         showPage(1);
 
         // Use event delegation, as these items are recreated later    
-        $(document).on("click", ".pagination li.current-page:not(.active)", function () {
+        $(document).on('click', '.pagination li.current-page:not(.active)', function () {
             return showPage(+$(this).text());
         });
-        $("#next-page").on("click", function () {
+
+        $('#next-page').on('click', function () {
             return showPage(currentPage + 1);
         });
 
-        $("#previous-page").on("click", function () {
+        $('#previous-page').on('click', function () {
             return showPage(currentPage - 1);
         });
     });
